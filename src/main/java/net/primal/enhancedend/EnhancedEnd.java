fc9ef0d2c3c5006733842ebdf3898c14ebfff543
Dr-Primal <mastersa003@gmail.com>
2022-09-17 21:11:45 -0400

    Initial commit

---
+++ src/main/java/net/primal/enhancedend/EnhancedEnd.java
@@ -0,0 +1,16 @@
+package net.primal.enhancedend;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EnhancedEnd implements ModInitializer {
+
+	public static final String MOD_ID = "enhancedend";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/primal/enhancedend/EnhancedEndClient.java
@@ -0,0 +1,10 @@
+package net.primal.enhancedend;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnhancedEndClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
